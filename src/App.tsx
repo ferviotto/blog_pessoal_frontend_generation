bf532b1da01bc7ba7829766f6b08a86d96329624
Fernanda_Viotto_De_Gobbi <fernandavdgobbi@gmail.com>
2024-08-05 10:06:15 -0300

    Criação Home React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://www.estadao.com.br/resizer/v2/CPAVWU2WLRBVFA6WU5BHTK5M2E.jpg?quality=80&auth=ea915c4ede2d3042d8e39b1c8f72a843cbc79dad83d5bbd7c45fa209a6213fcb&width=1200" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
